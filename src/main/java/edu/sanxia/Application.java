f20956ba90e58842ce42c29f80dfa9b0a387f36f
JmonkeyHuxy <237816542@qq.com>
2018-12-05 13:29:48 +0800

    Initial commit

---
+++ src/main/java/edu/sanxia/Application.java
@@ -0,0 +1,23 @@
+package edu.sanxia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description：
+ * 启动类
+ *
+ * @author Hu Xinyao 2018/12/3
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * 程序入口
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/edu/sanxia/configurations/DruidDataSourceConfiguration.java
@@ -0,0 +1,24 @@
+package edu.sanxia.configurations;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * description：
+ *
+ * @author Hu Xinyao 2018/12/5
+ */
+@Configuration
+public class DruidDataSourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource createDruidDataSource(){
+        return new DruidDataSource();
+    }
+
+}
